427cd7e1ae19406d2ccfecd4dd9e91dedcfee7e4
Riley Wong <rileynwong@gmail.com>
2022-02-21 01:29:20 -0600

    Add test that fails if someone other than domain owner tries to set record

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,22 @@
 const main = async () => {
+  // The first return is the deployer, the second is a random account
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
-};
+  console.log("Contract deployed by:", owner.address);
+
+  let txn = await domainContract.register("rrriley");
+  await txn.wait();
+
+  const domainAddress = await domainContract.getAddress("rrriley");
+  console.log("Owner of domain rrriley:", domainAddress);
+
+  // Trying to set a record that doesn't belong to me!
+  txn = await domainContract.connect(randomPerson).setRecord("rrriley", "domain get !");
+  await txn.wait();
+}
 
 const runMain = async () => {
   try {
@@ -16,4 +29,3 @@ const runMain = async () => {
 };
 
 runMain();
-
